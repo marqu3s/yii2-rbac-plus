6cb3fbdbf7b61ac3880497661cb400da33e03f34
Edmund Kawalec <e.kawalec@s4studio.pl>
2019-12-11 21:39:12 +0100

    PL translation

---
+++ src/messages/pl-PL/rbac.php
@@ -0,0 +1,75 @@
+<?php
+
+return [
+    /* =========== Controller common  =============*/
+    'Create new {0}' => 'Utwórz {0}',
+    'Have been create new {0} success' => 'Utworzono {0}',
+    'Update {0}' => 'Aktualizacja {0}',
+    'Have been update {0} success' => 'Zaktualizowano {0}',
+    'The requested page does not exist.'=>'Strona nie istnieje.',
+
+    /* =========== Views common  =============*/
+    /* 1. GridView */
+    '* Resize table columns just like a spreadsheet by dragging the column edges.' => '*Zmień rozmiar kolumn tabeli (podobnie jak w arkuszu kalkulacyjnym) przeciągając krawędzie kolumn.', // for toolbar buttons
+    'Reload Grid' => 'Odśwież', // for toolbar buttons
+    'All' => 'Wszystko', // for toolbar buttons
+    'Show all data' => 'Pokaż wszystko', // for toolbar buttons
+    'Page' => 'Strona', // for toolbar buttons
+    'Show first page data' => 'Pierwsza strona', // for action buttons
+    'View'=>'Pokaż',
+    'Update' => 'Aktualizuj', // for action buttons
+    'Delete' => 'Usuń', // for action buttons
+    /*2. Modal*/
+    'Close'=>'Zamknij',
+    'Save'=>'Zapisz',
+    'Edit'=>'Edycja',
+    'Ok'=>'OK',
+    'Cancel'=>'Anuluj',
+    'Create More'=>'Utwórz kolejne',
+    'Are you sure?'=> 'Jesteś pewny?',// for confirm delete
+    'Are you sure want to delete this item'=>'Czy na pewno chcesz usunąć ten element?',// for confirm delete
+    '(not use)'=>'(brak)',
+
+    /* =========== Auth Item Model  =============*/
+    'Name'=>'Nazwa',
+    'Description'=>'Opis',
+    'Rule Name'=>'Reguła',
+    'Data'=>'Data',
+    'Rule not exists'=>'Reguła nie istnieje',
+
+
+    /* =========== Rule Manager  =============*/
+    /* 1. Rule Model */
+    'Rule Name' => 'Reguła',// Rule Model
+    'Class Name'=>'Klasa',// Rule Model
+    'Class "{className}" not exist'=>'Klasa "{className}" nie istnieje',
+    'Class "{className}" must extends yii\rbac\Rule'=>'Klasa "{className}" musi dziedziczyć po yii\rbac\Rule',
+    'The "{className}::\$name" is not set'=>'"{className}::\$name" nie ustawiono',
+    'The "{className}::\$name" is incorrect with the name of rule you have set'=>'"{className}::\$name" jest niepoprawna w nazwie reguły, którą ustawiono',
+
+
+    /* 2. Rule Views */
+    'Rules Manager' => 'Menedżer reguł',
+
+
+    /* =========== Permisstion Manager  =============*/
+    /* 1. Permisstion Model */
+    'Permission name'=>'Nazwa uprawnienia',
+
+    /* 2. Permisstion Views */
+    'Permisstions Manager'=>'Menedżer uprawnień',
+
+
+    /* =========== Permisstion Manager  =============*/
+    /* 1. Permisstion Model */
+    'Role name'=>'Nazwa roli',
+
+    /* 2. Permisstion Views */
+    'Roles Manager'=>'Menedżer ról',
+
+    /* =========== User role assignemnt  =============*/
+    /* 1. Permisstion Views */
+    'User Assignment'=>'Przypisania',
+    'Assignment'=>'Przypisanie',
+];
+
